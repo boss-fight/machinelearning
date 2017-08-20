84f5b614bebdf091085d4904350fa4a3fdf4d781
Jeff Lusenhop <jeff.lusenhop@janova.us>
2017-08-20 13:35:29 -0400

    Changes to remove deprecation

--- MachineLearning1/MachineLearning1.py
+++ MachineLearning1/MachineLearning1.py
@@ -12,10 +12,10 @@ import math
 #print (clf.predict([[135,0]]))
 
 
-#totalOffenseHeaders = ["rank 2016", "team", "games", "points scored", "total yards", "offensive plays", "total yards per play", "turnovers lost", "fumbles lost", "total first downs", 
-#                       "passing completions", "passing attempts","passing yards","passing touchdowns","interceptions", "net yards per pass", "first downs passing", 
-#                       "attempted rushing plays", "yards rushing", "rushing touchdowns", "rushing yards per attempt", "rushing first downs", 
-#                       "penalties","penalty yards", "first downs by penalty", 
+#totalOffenseHeaders = ["rank 2016", "team", "games", "points scored", "total yards", "offensive plays", "total yards per play", "turnovers lost", "fumbles lost", "total first downs",
+#                       "passing completions", "passing attempts","passing yards","passing touchdowns","interceptions", "net yards per pass", "first downs passing",
+#                       "attempted rushing plays", "yards rushing", "rushing touchdowns", "rushing yards per attempt", "rushing first downs",
+#                       "penalties","penalty yards", "first downs by penalty",
 #                       "scoring drive percentage", "turnover percentage", "expected points contributed"]
 
 #Numpy Data gather
@@ -40,32 +40,29 @@ pd.set_option('display.width', 1000)
 
 pats2016 = pd.read_csv("NE2016.csv")
 
-gameHeaders = ["team score", "opp score", "O1stD", "OTotYd", "OPassY", "ORushY", "TO", 
-              "D1stD", "DTotYd", "DPassY", "DRushY", "DTO","Expected Offense Points", 
+gameHeaders = ["team score", "opp score", "O1stD", "OTotYd", "OPassY", "ORushY", "TO",
+              "D1stD", "DTotYd", "DPassY", "DRushY", "DTO","Expected Offense Points",
               "Expected Defense Points", "Expected Special Points"]
 
 #19
 newengland = []
 scores = []
 
-#pull data out and seperate into scores and 
+#pull data out and seperate into scores and
 for index, row in pats2016.iterrows():
     game = []
-    for x in range(10,22):
-        if x==10:
-            if math.isnan(row[x]):
-                print('bye week scores')
-            else:
-                scores.append(row[x])
+
+    for x in range(12,17):
+        if math.isnan(row[x]):
+            game.append(0)
         else:
-            if math.isnan(row[x]):
-                game.append(0)
-            else:
-                game.append(row[x])
+            game.append(row[x])
+
     if game[0]==0 and game[1] == 0:
         print('bye week game')
     else:
         newengland.append(game)
+        scores.append(row[10])
 
 for row in scores:
     print(row)
@@ -74,43 +71,54 @@ for row in newengland:
     print(row)
 
 
+
 #Create Support Vector Regressions
 #SVR is to predict next value in a series
 
 svr_lin = SVR(kernel='linear', C=1e3)
+print ('svr-lin')
 svr_poly = SVR(kernel='poly', C=1e3)
+print ('svr_poly')
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma = 0.1)
+print ('svr_rbf')
 
 #should we use Support Vector Machines as we are trying to predict scores?
 #setup Support Vector Regression
 svr_lin.fit(newengland,scores)
-svr_poly.fit(newengland,scores)
-svr_rbf.fit(newengland,scores)
+print ('fit svr_lin')
+#svr_poly.fit(newengland,scores)
+#print ('fit svr_poly')
+#svr_rbf.fit(newengland,scores)
+#print ('fit svr_rbf')
 
 
 #predict 2015 Redskins game 27-10
 print('2015 vs Redskins 27-10 win')
 
 print('Lin Fit')
-print(svr_lin.predict([10,27,460,299,161,2,16,250,213,37,2]))
+x = [27,460,299,161,2]
+x = np.reshape(x,(1, -1))
+print(svr_lin.predict(x))
 
-print('Poly Fit')
-print(svr_poly.predict([10,27,460,299,161,2,16,250,213,37,2]))
+#print('Poly Fit')
+#print(svr_poly.predict(x))
 
-print('RBF Fit')
-print(svr_rbf.predict([10,27,460,299,161,2,16,250,213,37,2]))
+#print('RBF Fit')
+#print(svr_rbf.predict(x))
 
 #predict 2015 Broncos game 24-30 loss
 print('2015 Broncos game 24-30 loss')
 
 print('Lin Fit')
-print(svr_lin.predict([30,16,301,262,39,1,23,433,254,179,1]))
+x = [16,101,262,39,1]
+x = np.reshape(x,(1, -1))
+print(svr_lin.predict(x))
 
-print('Poly Fit')
-print(svr_poly.predict([30,16,301,262,39,1,23,433,254,179,1]))
+#print('Poly Fit')
+#print(svr_poly.predict(x))
 
-print('RBF Fit')
-print(svr_rbf.predict([30,16,301,262,39,1,23,433,254,179,1]))
+#print('RBF Fit')
+#print(svr_rbf.predict(x))
 
 #team arrays
 #should we define teams as numbers in code?
