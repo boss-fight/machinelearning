66dbacc4a87663da9d496ada2bc42894c6a9ee88
boss-fight <bradleylusenhop@gmail.com>
2017-08-24 19:01:11 -0400

    data pull

--- MachineLearning1/MachineLearning1.py
+++ MachineLearning1/MachineLearning1.py
@@ -4,26 +4,8 @@ import numpy as np
 import pandas as pd
 import csv
 import math
-#fruit prediction
-#features = [[140,1], [130,1], [150,1], [170,0]]
-#labels = [0,0,1,1]
-#clf = tree.DecisionTreeClassifier()
-#clf = clf.fit(features,labels)
-#print (clf.predict([[135,0]]))
-
-
-#totalOffenseHeaders = ["rank 2016", "team", "games", "points scored", "total yards", "offensive plays", "total yards per play", "turnovers lost", "fumbles lost", "total first downs",
-#                       "passing completions", "passing attempts","passing yards","passing touchdowns","interceptions", "net yards per pass", "first downs passing",
-#                       "attempted rushing plays", "yards rushing", "rushing touchdowns", "rushing yards per attempt", "rushing first downs",
-#                       "penalties","penalty yards", "first downs by penalty",
-#                       "scoring drive percentage", "turnover percentage", "expected points contributed"]
-
-#Numpy Data gather
-#totalOffense = np.genfromtxt('teamOffense.csv',
-#                             dtype="i8,S5,i8,i8,i8,i8,f8,i8,i8,i8,i8,i8,i8,i8,i8,f8,i8,i8,i8,i8,f8,i8,i8,i8,i8,f8,f8,f8",
-#                             names= totalOffenseHeaders,
-#                             delimiter=',')
-#print(totalOffense)
+import os
+import glob
 
 
 #CSV files -> https://www.pro-football-reference.com/
@@ -38,54 +20,47 @@ pd.set_option('display.width', 1000)
 #print(totalOffense.iloc[0])
 
 
-pats2016 = pd.read_csv("NE2016.csv")
-
 gameHeaders = ["team score", "opp score", "O1stD", "OTotYd", "OPassY", "ORushY", "TO",
               "D1stD", "DTotYd", "DPassY", "DRushY", "DTO","Expected Offense Points",
               "Expected Defense Points", "Expected Special Points"]
 
-#19
-newengland = []
-scores = []
+
+
 
 #pull data out and seperate into scores and
-for index, row in pats2016.iterrows():
-    game = []
+#for index, row in pats2016.iterrows():
+#    game = []
 
-    for x in range(12,17):
-        if math.isnan(row[x]):
-            game.append(0)
-        else:
-            game.append(row[x])
+#    for x in range(12,17):
+#        if math.isnan(row[x]):
+#            game.append(0)
+#        else:
+#            game.append(row[x])
 
-    if game[0]==0 and game[1] == 0:
-        print('bye week game')
-    else:
-        newengland.append(game)
-        scores.append(row[10])
+#    if game[0]==0 and game[1] == 0:
+#        print('bye week game')
+#    else:
+#        newengland.append(game)
+#        scores.append(row[10])
 
-for row in scores:
-    print(row)
-
-for row in newengland:
-    print(row)
-
+ 
 
 
 #Create Support Vector Regressions
 #SVR is to predict next value in a series
 
-svr_lin = SVR(kernel='linear', C=1e3)
-print ('svr-lin')
-svr_poly = SVR(kernel='poly', C=1e3)
-print ('svr_poly')
-svr_rbf = SVR(kernel='rbf', C=1e3, gamma = 0.1)
-print ('svr_rbf')
+#svr_lin = SVR(kernel='linear', C=1e3)
+#print ('svr-lin')
+#svr_poly = SVR(kernel='poly', C=1e3)
+#print ('svr_poly')
+#svr_rbf = SVR(kernel='rbf', C=1e3, gamma = 0.1)
+#print ('svr_rbf')
 
 #should we use Support Vector Machines as we are trying to predict scores?
 #setup Support Vector Regression
-svr_lin.fit(newengland,scores)
-print ('fit svr_lin')
+
+#svr_lin.fit(newengland,scores)
+#print ('fit svr_lin')
 #svr_poly.fit(newengland,scores)
 #print ('fit svr_poly')
 #svr_rbf.fit(newengland,scores)
@@ -93,12 +68,12 @@ print ('fit svr_lin')
 
 
 #predict 2015 Redskins game 27-10
-print('2015 vs Redskins 27-10 win')
+#print('2015 vs Redskins 27-10 win')
 
-print('Lin Fit')
-x = [27,460,299,161,2]
-x = np.reshape(x,(1, -1))
-print(svr_lin.predict(x))
+#print('Lin Fit')
+#x = [27,460,299,161,2]
+#x = np.reshape(x,(1, -1))
+#print(svr_lin.predict(x))
 
 #print('Poly Fit')
 #print(svr_poly.predict(x))
@@ -107,12 +82,12 @@ print(svr_lin.predict(x))
 #print(svr_rbf.predict(x))
 
 #predict 2015 Broncos game 24-30 loss
-print('2015 Broncos game 24-30 loss')
+#print('2015 Broncos game 24-30 loss')
 
-print('Lin Fit')
-x = [16,101,262,39,1]
-x = np.reshape(x,(1, -1))
-print(svr_lin.predict(x))
+#print('Lin Fit')
+#x = [16,101,262,39,1]
+#x = np.reshape(x,(1, -1))
+#print(svr_lin.predict(x))
 
 #print('Poly Fit')
 #print(svr_poly.predict(x))
@@ -121,71 +96,181 @@ print(svr_lin.predict(x))
 #print(svr_rbf.predict(x))
 
 #team arrays
-#should we define teams as numbers in code?
+
 #1
-arizona = []
+#arizona = []
 #2
-atlanta = []
+#atlanta = []
 #3
-baltimore = []
+#baltimore = []
 #4
-buffalo = []
+#buffalo = []
 #5
-carolina = []
+#carolina = []
 #6
-chicago = []
+#chicago = []
 #7
-cincinnati = []
+#cincinnati = []
 #8
-cleveland = []
+#cleveland = []
 #9
-dallas = []
+#dallas = []
 #10
-denver = []
+#denver = []
 #11
-detroit = []
+#detroit = []
 #12
-greenbay = []
+#greenbay = []
 #13
-houston = []
+#houston = []
 #14
-indianapolis = []
+#indianapolis = []
 #15
-jacksonville = []
+#jacksonville = []
 #16
-kansascity = []
+#kansascity = []
 #17
-miami = []
+#losangeles = []
 #18
-minnesota = []
-
+#miami = []
+#19
+#minnesota = []
 #20
-neworleans = []
+#newengland = []
 #21
-newyorkg = []
+#neworleans = []
 #22
-newyorkj = []
+#newyorkg = []
 #23
-oakland = []
+#newyorkj = []
 #24
-philadelphia = []
+#oakland = []
 #25
-pittsburgh = []
+#philadelphia = []
 #26
-sandiego = []
+#pittsburgh = []
 #27
-sanfrancisco = []
+#sandiego = []
 #28
-seattle = []
+#sanfran = []
 #29
-stlouis = []
+#seattle = []
 #30
-tampabay = []
+#tampabay = []
 #31
-tennessee = []
+#tennessee = []
 #32
-washington = []
+#washington = []
 
-teams = [arizona,atlanta,baltimore,buffalo,carolina,chicago,cincinnati,cleveland,dallas,denver,detroit,greenbay,houston,indianapolis,
-         jacksonville,kansascity,miami,minnesota,newengland,neworleans,newyorkg,newyorkj,oakland,philadelphia,pittsburgh,sandiego,sanfrancisco,
-         seattle,stlouis,tampabay,tennessee,washington]
+#2015 stats
+arizona2015stats = []
+atlanta2015stats = []
+baltimore2015stats = []
+buffalo2015stats = []
+carolina2015stats = []
+chicago2015stats = []
+cincinnati2015stats = []
+cleveland2015stats = []
+dallas2015stats = []
+denver2015stats = []
+detroit2015stats = []
+greenbay2015stats = []
+houston2015stats = []
+indianapolis2015stats = []
+jacksonville2015stats = []
+kansascity2015stats = []
+losangeles2015stats = []
+miami2015stats = []
+minnesota2015stats = []
+newengland2015stats = []
+neworleans2015stats = []
+newyorkg2015stats = []
+newyorkj2015stats = []
+oakland2015stats = []
+philadelphia2015stats = []
+pittsburgh2015stats = []
+sandiego2015stats = []
+sanfran2015stats = []
+seattle2015stats = []
+tampabay2015stats = []
+tennessee2015stats = []
+washington2015stats = []
+#2015 scores
+arizona2015scores = []
+atlantas2015scores = []
+baltimore2015scores = []
+buffalo2015scores = []
+carolina2015scores = []
+chicago2015scores = []
+cincinnati2015scores = []
+cleveland2015scores = []
+dallas2015scores = []
+denver2015scores = []
+detroit2015scores = []
+greenbay2015scores = []
+houston2015scores = []
+indianapolis2015scores = []
+jacksonville2015scores = []
+kansascity2015scores = []
+losangeles2015scores = []
+miami2015scores = []
+minnesota2015scores = []
+newengland2015scores = []
+neworleans2015scores = []
+newyorkg2015scores = []
+newyorkj2015scores = []
+oakland2015scores = []
+philadelphia2015scores = []
+pittsburgh2015scores = []
+sandiego2015scores = []
+sanfran2015scores = []
+seattle2015scores = []
+tampabay2015scores = []
+tennessee2015scores = []
+washington2015scores = []
+
+
+
+directory2015 = './footballdata/2015/*.txt'
+
+teams2015 = [arizona2015stats,atlanta2015stats,baltimore2015stats,buffalo2015stats,carolina2015stats,chicago2015stats,cincinnati2015stats,cleveland2015stats,dallas2015stats,denver2015stats,
+             detroit2015stats,greenbay2015stats,houston2015stats,indianapolis2015stats,jacksonville2015stats,kansascity2015stats,losangeles2015stats,miami2015stats,minnesota2015stats,
+             newengland2015stats,neworleans2015stats,newyorkg2015stats,newyorkj2015stats,oakland2015stats,philadelphia2015stats,pittsburgh2015stats,sandiego2015stats,sanfran2015stats,
+         seattle2015stats,tampabay2015stats,tennessee2015stats,washington2015stats]
+
+
+scores2015 = [arizona2015scores ,atlantas2015scores,baltimore2015scores,buffalo2015scores,carolina2015scores,chicago2015scores,cincinnati2015scores,cleveland2015scores,dallas2015scores,denver2015scores,
+              detroit2015scores,greenbay2015scores,houston2015scores,indianapolis2015scores,jacksonville2015scores,kansascity2015scores,losangeles2015scores,miami2015scores,minnesota2015scores,
+              newengland2015scores,neworleans2015scores,newyorkg2015scores,newyorkj2015scores,oakland2015scores,philadelphia2015scores,pittsburgh2015scores,sandiego2015scores,sanfran2015scores,
+              seattle2015scores,tampabay2015scores,tennessee2015scores,washington2015scores]
+
+def getData(directory,teams,scores):
+    i = 0
+    for file in glob.iglob(directory):
+        print(file)
+        seasonfile = pd.read_csv(file)
+        season = []
+        seasonscores = []
+        for index, row in seasonfile.iterrows():
+            game = []
+            for x in range(12,17):
+                if math.isnan(row[x]):
+                    game.append(0)
+                else:
+                    game.append(row[x]) 
+        
+            if game[0]==0 and game[1] == 0:
+                print('bye week game')
+            else:
+                season.append(game)
+                seasonscores.append(row[10])
+        teams[i].append(season)
+        scores[i].append(seasonscores)
+        print("team 2015")
+        print(teams[i])
+        print("scores 2015")
+        print(scores[i])
+        print(i)
+        i += 1
+
+getData(directory2015,teams2015, scores2015)
